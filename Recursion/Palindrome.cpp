3e18695594c0c394201dc590f9f1098311518b5d
Anisha Ramanuj <98104395+AnishaRamanuj@users.noreply.github.com>
2022-11-17 12:21:50 +0530

    Create Palindrome.cpp

---
+++ Recursion/Palindrome.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool fun(int i, string &s){
+  int n = s.size();
+  if(i >= n/2) return true;
+  if(s[i], s[n-i-1])return false;
+return fun(i+1, s);
+}
+
+int main(){
+  string s = "madam";
+  cout << fun(0,s);
+  return 0;
+}
