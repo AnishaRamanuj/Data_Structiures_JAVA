09270ab6e7a1c0aa35e4b9c9a8c3a458875d3bd8
Anisha Ramanuj <98104395+AnishaRamanuj@users.noreply.github.com>
2022-11-17 12:23:53 +0530

    Update Palindrome.cpp

--- Recursion/Palindrome.cpp
+++ Recursion/Palindrome.cpp
@@ -1,10 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
 #include<bits/stdc++.h>
 using namespace std;
 
 bool fun(int i, string &s){
   int n = s.size();
   if(i >= n/2) return true;
-  if(s[i], s[n-i-1])return false;
+  if(s[i] != s[n-i-1])return false;
 return fun(i+1, s);
 }
 
